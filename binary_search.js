1177a5163a9514ebce94820558404b41ce3d585c
Spencer Cornelia <cornelia.spencer@yahoo.com>
2015-07-24 23:08:57 -0700

    binary search written

--- binary_search.js
+++ binary_search.js
@@ -1,1 +1,17 @@
+function binarySearch(items, value) {
+	var startIndex = 0;
+	var stopIndex = items.length-1;
+	var middleIndex = Math.floor((stopIndex - startIndex)/2);
 
+	while(items[middleIndex] != value && startIndex < stopIndex) {
+		if (value < items[middle]) {
+			stopIndex = middleIndex - 1;
+		} else if (value > items[middle]) {
+			startIndex = middle + 1;
+		}
+
+		middle = Math.floor((stopIndex + startIndex)/2);
+	}
+
+	return (items[middle] != value) ? -1 : middle;
+}
